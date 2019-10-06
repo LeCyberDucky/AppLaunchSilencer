f4b75f7a78bcc1e1ccc6f6511014e453adc67d25
Andy <MasterSumo123@Yahoo.de>
2019-10-06 18:40:22 +0200

    Still comin' for ya, Forza!

--- AppLaunchSilencer.py
+++ AppLaunchSilencer.py
@@ -1,16 +1,21 @@
 # Damn you Forza. Why are you so loud?
 
+import os
 from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
 import time
 
 
 with open("Settings.config") as inFile:
-    settings = dict([line.split(":") for line in inFile])
+    settings = dict([line.split(": ") for line in inFile])
 
+settings["launcher path"] = settings["launcher path"].strip()
 settings["process"] = settings["process"].strip()
 settings["launch volume"] = float(settings["launch volume"])/100
 settings["target volume"] = float(settings["target volume"])/100
 settings["delay"] = int(settings["delay"])
+
+# Execute wanted program
+os.startfile(settings["launcher path"])
 
 processFound = False
 
@@ -26,4 +31,9 @@ volume.SetMasterVolume(settings["launch volume"], None)
 
 time.sleep(settings["delay"])
 
-volume.SetMasterVolume(settings["target volume"], None)
+# Make sure process wasn't closed while sleeping
+sessions = AudioUtilities.GetAllSessions()
+for session in sessions:
+    if session.Process and session.Process.name() == settings["process"]:
+        volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+        volume.SetMasterVolume(settings["target volume"], None)
